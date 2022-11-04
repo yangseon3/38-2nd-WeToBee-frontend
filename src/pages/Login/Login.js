f5d0b0a2e97359dfd8653240c050d4df203a43f5
daylee619 <110576332+daylee619@users.noreply.github.com>
2022-11-04 19:29:54 +0900

    스타일드 컴포넌트 컨벤션 통합 수정 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,45 +11,47 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <Wrapper>
-        <Signin>회원가입 with kakao</Signin>
-        <Img
+    <S.Container>
+      <S.Wrapper>
+        <S.Signin>회원가입 with kakao</S.Signin>
+        <S.Img
           src="/images/kakao_login_large_narrow.png"
           alt="img "
           onClick={handleLogin}
         />
-      </Wrapper>
-    </Container>
+      </S.Wrapper>
+    </S.Container>
   );
 };
 
 export default Login;
 
-const Signin = styled.div`
-  font-size: 30px;
-  margin-bottom: 40px;
-  text-align: center;
-`;
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  border: 1px solid black;
-`;
-const Img = styled.img`
-  width: 350px;
-`;
+const S = {
+  Signin: styled.div`
+    font-size: 30px;
+    margin-bottom: 40px;
+    text-align: center;
+  `,
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    border: 1px solid black;
+  `,
+  Img: styled.img`
+    width: 350px;
+  `,
 
-const Wrapper = styled.div`
-  width: 500px;
-  margin: 0 auto;
-  padding: 80px 20px;
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+  Wrapper: styled.div`
+    width: 500px;
+    margin: 0 40px 200px 0;
+    padding: 80px 20px;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+};
